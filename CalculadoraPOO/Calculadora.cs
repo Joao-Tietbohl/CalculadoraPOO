d21f0f427f3c276bab91966ef32dfdfa4e910f8f
Joao Pedro T. Correa <joaopedrotcorrea1212@gmail.com>
2022-03-11 18:31:05 -0300

    Add project files.

---
+++ CalculadoraPOO/Calculadora.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPOO
+{
+    class Historico
+    {
+        public decimal primeiroNumero;
+        public decimal segundoNumero;
+        public string op;
+        public decimal resultado;
+    }
+
+    internal class Calculadora
+    {
+       public Historico[] historicoOperacoes;
+
+        public Calculadora()
+        {
+
+        }
+
+        public decimal adicao(decimal a, decimal b)
+        {
+            decimal resultado = a + b;
+            CriaEntrada(a, b, "+", resultado);
+            return resultado;
+        }
+
+        private void CriaEntrada(decimal a, decimal b, string op, decimal resultado)
+        {
+            Historico entrada = new Historico();
+            entrada.op = op;
+            entrada.primeiroNumero = a;
+            entrada.segundoNumero = b;
+            entrada.resultado = resultado;
+
+            for(int i = 0; i < 10; i++)
+            {
+                if (historicoOperacoes[i] == null)
+                {
+                    historicoOperacoes[i] = entrada;
+                    break;
+                }
+            }
+        }
+
+        public decimal subtracao(decimal a, decimal b)
+        {
+            decimal resultado = a + b;
+            CriaEntrada(a, b, "-", resultado);
+            return resultado;
+        }
+
+        public decimal multiplicacao(decimal a, decimal b)
+        {
+            decimal resultado = a * b;
+            CriaEntrada(a, b, "*", resultado);
+            return resultado;
+        }
+
+        public decimal divisao(decimal a, decimal b)
+        {
+            decimal resultado = a / b;
+            CriaEntrada(a, b, "/", resultado);
+            return resultado;
+            
+        }
+
+        public void VisualizarHistorico()
+        {
+            foreach (Historico item in historicoOperacoes)
+            {
+                if (item != null)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(item.primeiroNumero + " " + item.op + " " + item.segundoNumero + " = " + item.resultado);
+
+                }
+            }
+        }
+    }
+}
---
+++ CalculadoraPOO/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace CalculadoraPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao = 0;
+            decimal primeiroNumero = 0, segundoNumero = 0;
+            Calculadora calculadora = new Calculadora();
+            calculadora.historicoOperacoes = new Historico[10];
+
+            while (opcao != 6)
+            {
+                Menu(out opcao,ref primeiroNumero,ref segundoNumero, calculadora);
+
+                Operacoes(opcao, primeiroNumero, segundoNumero, calculadora);               
+            }
+        }
+
+        private static void OpcaoEhCinco(int opcao, Calculadora calculadora)
+        {
+             calculadora.VisualizarHistorico();
+            
+        }
+
+        private static bool Menu(out int opcao,ref decimal primeiroNumero,ref decimal segundoNumero, Calculadora calculadora)
+        {
+            opcao = 0;
+            while (opcao != 1 && opcao != 2 && opcao != 3 && opcao != 4 && opcao != 5 && opcao != 6)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Qual operação você quer realizar?");
+                Console.WriteLine("1 - Somar");
+                Console.WriteLine("2 - Subtração");
+                Console.WriteLine("3 - Multiplicação");
+                Console.WriteLine("4 - Divisão");
+                Console.WriteLine("5 - Visualizar historico de operações");
+                Console.WriteLine("6 - Sair");
+                opcao = Int32.Parse(Console.ReadLine());
+            }
+            if ( opcao == 6)
+            {
+                return true;
+            }
+
+            if( opcao == 5)
+            {
+                OpcaoEhCinco(opcao, calculadora);
+                return true;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o primeiro número");
+            primeiroNumero = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número");
+            segundoNumero = decimal.Parse(Console.ReadLine());
+           
+            while (opcao == 4 && segundoNumero == 0)
+            {
+                Console.WriteLine("Erro! Não existe divisão por zero. Digite outro número");
+                segundoNumero = decimal.Parse(Console.ReadLine());
+            }
+
+            return true;
+        }
+
+
+
+        private static void Operacoes(int opcao, decimal primeiroNumero, decimal segundoNumero, Calculadora calculadora)
+        {
+            switch (opcao)
+            {
+                case 1:
+                    calculadora.adicao(primeiroNumero, segundoNumero);
+                    break;
+
+                case 2:
+                    calculadora.subtracao(primeiroNumero, segundoNumero);
+                    break;
+
+                case 3:
+                    calculadora.multiplicacao(primeiroNumero, segundoNumero);
+                    break;
+
+                case 4:
+                    calculadora.divisao(primeiroNumero, segundoNumero);
+                    break;
+            }
+        }        
+    }       
+}
